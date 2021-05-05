a58cd6b4cc14811e12f82a95c1eea498e504446d
developer-pseudo <71831643+developer-pseudo@users.noreply.github.com>
2021-05-05 08:21:09 +0300

    Remove comments as everything I've typed is documented in asio documentation

--- UnbearableTrials/unbearabletrials.cpp
+++ UnbearableTrials/unbearabletrials.cpp
@@ -1,67 +1,62 @@
-// includes
 #include <iostream>
 #include <chrono>
 
-// windows will scream at you if you dont add this since every version interpretates networking differently
 #ifdef _WIN32
 #define _WIN32_WINNT 0x0A00
 #endif
 
-// asio includes
 #define ASIO_STANDALONE
 #include <asio.hpp>
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
-std::vector<char> vBuffer(1 * 1024); // create a reasonably large buffer since we have no idea how big the data might be
+std::vector<char> vBuffer(1 * 1024);
 
-void GrabSomeData(asio::ip::tcp::socket& socket) { // create a function to grab data from our socket
+void GrabSomeData(asio::ip::tcp::socket& socket) {
 	socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
-		[&](std::error_code ec, std::size_t length) { // prototype of a lambda function
-			if (!ec) { // if there is no errors we ...
-				std::cout << "\n\nread " << length << " bytes\n\n"; // ... send the length of bytes that we got from the server
+		[&](std::error_code ec, std::size_t length) {
+			if (!ec) {
+				std::cout << "\n\nread " << length << " bytes\n\n";
 
-				for (int i = 0; i < length; i++) // a wild for loop has appeared!
-					std::cout << vBuffer[i]; // dont forget to print out the data :smart:
+				for (int i = 0; i < length; i++)
+					std::cout << vBuffer[i];
 
-				GrabSomeData(socket); // no this is not a recursive nightmare, it just primes it so it runs in the background :smart:
+				GrabSomeData(socket);
 			}
 		}
 	);
 }
 
-int main() { // the main function of our program
-	asio::error_code ec; // use ec to get specific errors
-	asio::io_context context; // create our platform specific interface context
-	asio::io_context::work idleWork(context); // torture our context so asio doesnt finish
+int main() {
+	asio::error_code ec; 
+	asio::io_context context;
+	asio::io_context::work idleWork(context);
 
-	std::thread thrContext = std::thread([&]() {context.run(); }); // run our context in its own thread so it wont block our program
+	std::thread thrContext = std::thread([&]() {context.run(); });
 
-	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("51.38.81.49", ec), 80); // declare where asio expects people to connect to (im using the ip address of https://example.com)
-	asio::ip::tcp::socket socket(context); // create our socket which context will implement
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("51.38.81.49", ec), 80);
+	asio::ip::tcp::socket socket(context);
 
 	std::cout << "attempting to connect" << std::endl;
-	socket.connect(endpoint, ec); // try connecting to our TCP server
+	socket.connect(endpoint, ec);
 
-	// check for connection errors
 	if (!ec) {
 		std::cout << "connected to the socket" << std::endl;
 	}
 	else {
-		std::cout << "failed to connect to the socket, error message below" << std::endl << ec.message() << std::endl; // send our error so we know what happened
+		std::cout << "failed to connect to the socket, error message below" << std::endl << ec.message() << std::endl;
 	}
 
-	if (socket.is_open()) { // check if the socket is opened
-		GrabSomeData(socket); // prime our data getter function BEFORE we send the request
+	if (socket.is_open()) {
+		GrabSomeData(socket);
 
-		std::string sRequest = // send a request to a server
+		std::string sRequest =
 			"GET /index.html HTTP/1.1\r\n"
 			"Host: david-barr.co.uk\r\n"
 			"Connection: close\r\n\r\n";
 
-		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec); // send as much data as the socket can
+		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
 
-		// give our program some sleep pills ;)
 		using namespace std::chrono_literals;
 		std::this_thread::sleep_for(20000ms);
 	}
